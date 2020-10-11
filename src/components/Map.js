b1c12de6721c48476517ffe0eb376cb169b583ba
Ville Häyrynen <hayrynenville@outlook.com>
2020-10-11 12:01:41 +0300

    Added markers to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import L, { marker } from 'leaflet';
+import L from 'leaflet';
 import styled from 'styled-components';
 import 'leaflet/dist/leaflet.css';
 import icon from '../mapIcon.png'
@@ -41,7 +41,65 @@ export default class Map extends React.Component {
             maxZoom: 20,
             maxNativeZoom: 17,
         }).addTo(this.map);
+        //Oulu
         L.marker([65.055363, 25.456706], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Rovaniemi
+        L.marker([66.498664, 25.704456], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Kajaani
+        L.marker([64.220400, 27.738443], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Kuusamo
+        L.marker([65.956149, 29.160340], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Kokkola
+        L.marker([63.830086, 23.147643], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Kuopio
+        L.marker([62.896749, 27.682195], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Joensuu
+        L.marker([62.600700, 29.775881], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Jyväskylä
+        L.marker([62.243627, 25.755759], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Vaasa
+        L.marker([63.098472, 21.619318], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Tampere
+        L.marker([61.477822, 23.767594], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Pori
+        L.marker([61.478007, 21.787210], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Tampere
+        L.marker([61.477822, 23.767594], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Turku
+        L.marker([60.453800, 22.253627], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Lahti
+        L.marker([60.977311, 25.656931], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Kouvola
+        L.marker([60.866513, 26.705088], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Lappeenranta
+        L.marker([61.049776, 28.197684], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Kotka
+        L.marker([60.461012, 26.938076], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Helsinki
+        L.marker([60.171106, 24.943311], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Espoo
+        L.marker([60.206013, 24.659662], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup('Hello');
+        //Kemi
+        L.marker([65.734932, 24.568858], {icon: mapIcon}).addTo(this.map)
                             .bindPopup('Hello');
     }
 
