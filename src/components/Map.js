8ba94ef1e81d5870b4b4c39243478bcc30921e2d
Ville Häyrynen <hayrynenville@outlook.com>
2020-10-13 13:07:04 +0300

    Added charger-data.json and made Maps.js and Stations.js utilize it.

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,6 +3,7 @@ import L from 'leaflet';
 import styled from 'styled-components';
 import 'leaflet/dist/leaflet.css';
 import icon from './mapIcon.png'
+import chargerData from '../charger-data.json'
 
 const Wrapper = styled.div`
     margin-left: auto;
@@ -39,69 +40,14 @@ export default class Map extends React.Component {
             maxZoom: 20,
             maxNativeZoom: 17,
         }).addTo(this.map);
+        var stations = [];
+        stations.push(chargerData.chargers)
+        console.log(stations)
 
-        
-        //Oulu
-        L.marker([65.055363, 25.456706], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('OULU Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Rovaniemi
-        L.marker([66.498664, 25.704456], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('ROVANIEMI Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Kajaani
-        L.marker([64.220400, 27.738443], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('KAJAANI Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Kuusamo
-        L.marker([65.956149, 29.160340], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('KUUSAMO Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Kokkola
-        L.marker([63.830086, 23.147643], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('KOKKOLA Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Kuopio
-        L.marker([62.896749, 27.682195], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('KUOPIO Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Joensuu
-        L.marker([62.600700, 29.775881], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('JOENSUU Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Jyväskylä
-        L.marker([62.243627, 25.755759], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('JYVÄSKYLÄ Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Vaasa
-        L.marker([63.098472, 21.619318], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('VAASA Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Tampere
-        L.marker([61.477822, 23.767594], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('TAMPERE Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Pori
-        L.marker([61.478007, 21.787210], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('PORI Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Turku
-        L.marker([60.453800, 22.253627], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('TURKU Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Lahti
-        L.marker([60.977311, 25.656931], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('LAHTI Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Kouvola
-        L.marker([60.866513, 26.705088], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('KOUVOLA Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Lappeenranta
-        L.marker([61.049776, 28.197684], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('LAPPEENRANTA Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Kotka
-        L.marker([60.461012, 26.938076], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('KOTKA Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Helsinki
-        L.marker([60.171106, 24.943311], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('HELSINKI Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Espoo
-        L.marker([60.206013, 24.659662], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('ESPOO Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Kemi
-        L.marker([65.734932, 24.568858], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('KEMI Charge station<br>Fast charge status:<br>Slow charge status:');
-        //Kemijärvi
-        L.marker([66.717135, 27.427773], {icon: mapIcon}).addTo(this.map)
-                            .bindPopup('KEMIJÄRVI Charge station<br>Fast charge status:<br>Slow charge status:');
-        
+        stations[0].map(station => (
+            L.marker([station.lat, station.lng], {icon: mapIcon}).addTo(this.map)
+                            .bindPopup(station.name + ' Charge station<br>Fast charge status: ' + station.faststatus + '<br>Slow charge status: ' + station.slowstatus)
+        ))
     }
 
     render(){
--- src/routes/Stations.js
+++ src/routes/Stations.js
@@ -1,122 +1,25 @@
 import React from 'react'
 import styles from './Stations.module.css'
+import chargerData from '../charger-data.json'
 
 
 
-var stations = [
-    {
-        "name" : "Oulu",
-        "slowcharge" : "Free",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Rovaniemi",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Kajaani",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Kuusamo",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Kokkola",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Taken"
-    },
-    {
-        "name" : "Kuopio",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Joensuu",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Jyväskylä",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Vaasa",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Tampere",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Taken"
-    },
-    {
-        "name" : "Pori",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Turku",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Lahti",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Kouvola",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Lappeenranta",
-        "slowcharge" : "Free",
-        "fastcharge" : "Taken"
-    },
-    {
-        "name" : "Kotka",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Helsinki",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Espoo",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Kemi",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    },
-    {
-        "name" : "Kemijärvi",
-        "slowcharge" : "Taken",
-        "fastcharge" : "Free"
-    }
-]
+var stations = [];
+stations.push(chargerData.chargers)
+console.log(stations)
 
 const Test = ({stations}) => (
+    
         <div className={ styles.container }>
-        {stations.map(station => (
+        {stations[0].map(station => (
             <>
             <div className={ styles.supercontainer }>
             <div className={ styles.station } >
                 <div className={ styles.stationInfoBox}>
                     <div className={ styles.stationName } key={station.name}>{station.name}</div>
                     <div className={ styles.stationInfo }>
-                        <div className={ styles.stationItem }>Slow chargestation status: {station.slowcharge}</div>
-                        <div className={ styles.stationItem }>Fast chargestation status: {station.fastcharge}</div>
+                        <div className={ styles.stationItem }>Slow chargestation status: {station.slowstatus}</div>
+                        <div className={ styles.stationItem }>Fast chargestation status: {station.faststatus}</div>
                     </div>
                 </div>
             </div>
